4352d05aee3f7f1065763a70fac3c233dd522e16
Weslley Addson <wesquel@hotmail.com>
2024-02-10 19:09:34 -0300

    Enabled config server

--- java/cloud_spring_server/src/main/java/com/dev_addson/cloud_spring_server/CloudSpringServerApplication.java
+++ java/cloud_spring_server/src/main/java/com/dev_addson/cloud_spring_server/CloudSpringServerApplication.java
@@ -2,12 +2,15 @@ package com.dev_addson.cloud_spring_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
+@EnableConfigServer
 public class CloudSpringServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CloudSpringServerApplication.class, args);
 	}
+	
 
 }
