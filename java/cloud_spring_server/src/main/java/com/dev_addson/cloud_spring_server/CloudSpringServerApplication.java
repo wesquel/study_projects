4a4d8bc62acf4b234f917e56b08c026d2419aaec
Weslley Addson <wesquel@hotmail.com>
2024-02-10 18:58:19 -0300

    init commit

---
+++ java/cloud_spring_server/src/main/java/com/dev_addson/cloud_spring_server/CloudSpringServerApplication.java
@@ -0,0 +1,13 @@
+package com.dev_addson.cloud_spring_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudSpringServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudSpringServerApplication.class, args);
+	}
+
+}
---
+++ java/cloud_spring_server/src/test/java/com/dev_addson/cloud_spring_server/CloudSpringServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dev_addson.cloud_spring_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudSpringServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
