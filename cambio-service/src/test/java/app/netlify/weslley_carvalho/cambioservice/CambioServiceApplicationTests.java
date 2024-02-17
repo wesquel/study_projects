d94924f147a326554d4487c8ad2f3d0746a177e1
Weslley Addson <wesquel@hotmail.com>
2024-02-17 01:02:07 -0300

    init cambio-service

---
+++ cambio-service/src/test/java/app/netlify/weslley_carvalho/cambioservice/CambioServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package app.netlify.weslley_carvalho.cambioservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CambioServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
