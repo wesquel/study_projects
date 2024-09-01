fe7507b647be4ae0e3e770a27ad50db6f1faa749
Weslley Addson <wesquel@hotmail.com>
2024-09-01 19:51:54 -0300

    feature: Create role initializer o facilitate project creation.

---
+++ java/authentication/src/main/java/com/addsonweslley/autenticacao/config/DataInitializer.java
@@ -0,0 +1,22 @@
+package com.addsonweslley.autenticacao.config;
+
+import com.addsonweslley.autenticacao.models.Role;
+import com.addsonweslley.autenticacao.repositorys.RoleRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataInitializer {
+
+    @Bean
+    CommandLineRunner initDatabase(RoleRepository roleRepository) {
+        return args -> {
+            if (roleRepository.count() == 0) {
+                roleRepository.save(new Role("USER"));
+                roleRepository.save(new Role("ADMIN"));
+                roleRepository.save(new Role("TEACHER"));
+            }
+        };
+    }
+}
