ba138762f37af11da78f62c3002fb81b9eb5a73e
Weslley Addson <wesquel@hotmail.com>
2024-09-14 12:50:40 -0300

    feature: Create App access for users.

--- java/authentication/src/main/java/com/addsonweslley/autenticacao/dto/User/UserResponse.java
+++ java/authentication/src/main/java/com/addsonweslley/autenticacao/dto/User/UserResponse.java
@@ -1,11 +1,15 @@
 package com.addsonweslley.autenticacao.dto.User;
 
+import com.addsonweslley.autenticacao.models.App;
+import com.addsonweslley.autenticacao.models.Role;
 import com.addsonweslley.autenticacao.models.User;
 
-public record UserResponse(String name, String username, String email) {
+import java.util.Set;
+
+public record UserResponse(String name, String username, String email, Set<Role> roles, Set<App> app_access) {
 
     public static UserResponse fromUser(User user) {
-        return new UserResponse(user.getName(), user.getUsername(), user.getEmail());
+        return new UserResponse(user.getName(), user.getUsername(), user.getEmail(), user.getRoles(), user.getAppAccess());
     }
 
 }
